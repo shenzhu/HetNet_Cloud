96d3123c9f7282a9b8a87963fc064687e5e3d763
lanking520 <lanking520@gmail.com>
2017-03-28 14:24:32 -0400

    updates

--- webserver/table.py
+++ webserver/table.py
@@ -1,3 +1,6 @@
 class table:
 	LOGIN = "CREATE TABLE IF NOT EXISTS login (device_id text, password text NOT NULL, email text NOT NULL,PRIMARY KEY (device_id));"
-	NETWORK = "CREATE TABLE IF NOT EXISTS networks (ssid text, bandwidth text NOT NULL, AvgSS integer NOT NULL,device_id text REFERENCES login, PRIMARY KEY (ssid,location));"
+	NETWORK = "CREATE TABLE IF NOT EXISTS networks (ssid text, bandwidth text NOT NULL, security text NOT NULL, location text NOT NULL,AvgSS integer NOT NULL,device_id text REFERENCES login, PRIMARY KEY (ssid,location));"
+	APPLICATION = "CREATE TABLE IF NOT EXISTS application (name text, device_id text REFERENCES login,PRIMARY KEY (name,device_id));"
+	APPDETL = "CREATE TABLE IF NOT EXISTS appdetl (access_time date, type text, name text REFERENCES application, value numeric, device_id text REFERENCES login,PRIMARY KEY (AccessTime, type));"
+	# Type indicate the CPU/Battery/DataUsage
