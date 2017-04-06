719f47c00d9d47cf120e093c545f29f18527afc2
Shen Zhu <shenzhu0127@gmail.com>
2017-04-05 22:27:12 -0400

    fix some bugs

--- webserver/routes/event.py
+++ webserver/routes/event.py
@@ -1,8 +1,9 @@
-from flask import render_template,Response
+from flask import render_template, Response
 from . import routes
 from server import *
 import json
 from flask import request
+
 
 @routes.route('/uploadnetwork', methods=['POST'])
 def upload_network_data():
@@ -24,18 +25,18 @@ def upload_network_data():
 
     if len(loginRows) > 0:
         # Insert to database
-        networkCursor = g.conn.execute('INSERT INTO networks(ssid, bandwidth, security, location, avgss, device_id) VALUES(%s, %s, %s, %s, %s, %s)',
-                                       ssid, bandwidth, security, location, avgss, device_id)
+        networkCursor = g.conn.execute(
+            'INSERT INTO networks(ssid, bandwidth, security, location, avgss, device_id) VALUES(%s, %s, %s, %s, %s, %s)',
+            ssid, bandwidth, security, location, avgss, device_id)
         responseJSON = {"status": "Success"}
     else:
         responseJSON = {"status": "Failure"}
         print "ERROR! No corresponding primary key"
 
-
     return Response(response=json.dumps(responseJSON), status=200, mimetype="application/json")
 
 
-@routes.route('/uploadlogin', methods = ['POST'])
+@routes.route('/uploadlogin', methods=['POST'])
 def upload_login_data():
     loginDataJSON = request.get_json()
 
@@ -45,13 +46,14 @@ def upload_login_data():
     email = loginDataJSON['email']
 
     # Insert into database
-    cursor = g.conn.execute('INSERT INTO login(device_id, password, email) VALUES(%s, %s, %s)', device_id, password, email)
+    cursor = g.conn.execute('INSERT INTO login(device_id, password, email) VALUES(%s, %s, %s)', device_id, password,
+                            email)
     responseJSON = {"status": "Success"}
 
     return Response(response=json.dumps(responseJSON), status=200, mimetype="application/json")
 
 
-@routes.route('/uploadapplication', methods = ['POST'])
+@routes.route('/uploadapplication', methods=['POST'])
 def upload_application_data():
     applicationDataJSON = request.get_json()
 
@@ -68,7 +70,7 @@ def upload_application_data():
     # Insert into database
     if len(loginRows) > 0:
         applicationCursor = g.conn.execute('INSERT INTO application(name, device_id) VALUES(%s, %s)',
-                                            name, device_id)
+                                           name, device_id)
         responseJSON = {"status": "Success"}
     else:
         responseJSON = {"status": "Failure"}
@@ -77,7 +79,7 @@ def upload_application_data():
     return Response(response=json.dumps(responseJSON), status=200, mimetype="application/json")
 
 
-@routes.route('/uploadappdetl', methods = ['POST'])
+@routes.route('/uploadappdetl', methods=['POST'])
 def upload_appdetl_data():
     appdetlDataJSON = request.get_json()
 
@@ -97,8 +99,9 @@ def upload_appdetl_data():
 
     # Insert into database
     if len(applicationRows) > 0:
-        appdetlCursor = g.conn.execute('INSERT INTO appdetl(access_time, type, name, interval, value, device_id) VALUES(%s, %s, %s, %s, %s, %s)',
-                                       access_time, type, name, interval, value, device_id)
+        appdetlCursor = g.conn.execute(
+            'INSERT INTO appdetl(access_time, type, name, interval, value, device_id) VALUES(%s, %s, %s, %s, %s, %s)',
+            access_time, type, name, interval, value, device_id)
         responseJSON = {"status": "Success"}
     else:
         responseJSON = {"status": "Failure"}
@@ -130,7 +133,7 @@ def register_new_user():
     # Insert into database
     if len(results) == 0:
         g.conn.execute('INSERT INTO login(device_id, password, email) VALUES(%s, %s, %s)',
-                    device_id, password, email)
+                       device_id, password, email)
         responseJSON = {"status": "Success"}
     else:
         responseJSON = {"status": "Failure"}
@@ -146,108 +149,121 @@ def table_name_extracter(name):
         names.append(row[0])
     return names
 
+
 @routes.route('/coach')
 def coach():
     return render_template("coach.html")
 
 
-@routes.route('/coach/gethalls',methods=['GET'])
+@routes.route('/coach/gethalls', methods=['GET'])
 def halls():
-    halls_data=[]
+    halls_data = []
     cursor = g.conn.execute('SELECT * from hall;')
     for row in cursor:
-        temp = {'location':'','name':'','capacity':''}
+        temp = {'location': '', 'name': '', 'capacity': ''}
         temp['location'] = row[0]
         temp['name'] = row[1]
         temp['capacity'] = row[2]
         halls_data.append(temp)
-    resp = Response(response=json.dumps(halls_data),status=200, mimetype="application/json")
-    return(resp)
-    
-@routes.route('/coach/getavailablehall',methods=['POST'])
+    resp = Response(response=json.dumps(halls_data), status=200, mimetype="application/json")
+    return (resp)
+
+
+@routes.route('/coach/getavailablehall', methods=['POST'])
 def available_hall():
     data = request.get_json()
     availability = []
     cursor = g.conn.execute('SELECT timeslot from instruction where name = %s', data['name'])
     for row in cursor:
         availability.append(row[0])
-    resp = Response(response=json.dumps(availability),status=200, mimetype="application/json")
-    return(resp)
+    resp = Response(response=json.dumps(availability), status=200, mimetype="application/json")
+    return (resp)
 
-@routes.route('/coach/gettrain',methods=['POST'])
+
+@routes.route('/coach/gettrain', methods=['POST'])
 def trainning():
     data = request.get_json()
     trainning_data = []
-    cursor = g.conn.execute('select time, date, coaid, name from train inner join member on (train.pid = member.pid) where coaid = %s;', data['coaid'])
+    cursor = g.conn.execute(
+        'select time, date, coaid, name from train inner join member on (train.pid = member.pid) where coaid = %s;',
+        data['coaid'])
     for row in cursor:
-        temp = {'time':'','date':'','coaid':'','Member':''}
+        temp = {'time': '', 'date': '', 'coaid': '', 'Member': ''}
         temp['time'] = row[0].strftime('%H:%M:%S')
         temp['date'] = row[1].strftime('%Y-%m-%d')
         temp['coaid'] = row[2]
         temp['Member'] = row[3]
         trainning_data.append(temp)
-    resp = Response(response=json.dumps(trainning_data),status=200, mimetype="application/json")
-    return(resp)
+    resp = Response(response=json.dumps(trainning_data), status=200, mimetype="application/json")
+    return (resp)
+
 
 def checkcid(cid):
     cursor = g.conn.execute('SELECT cid from course where cid = %s', cid)
     existed = False
-    for row in cursor: 
+    for row in cursor:
         existed = True
     return existed
 
-@routes.route('/coach/addinstruction',methods=['POST'])
+
+@routes.route('/coach/addinstruction', methods=['POST'])
 def addinstruction():
     data = request.get_json()
-    if not check_time_hall(data['week'],data['time'],data['hall']):
-        cursor = g.conn.execute('INSERT INTO instruction VALUES(%s,%s,%s,%s,%s)', data['coaid'],data['course'],data['hall'],data['week'],data['time'])
-        resp = Response(response=json.dumps([{'result':'Success'}]),status=200, mimetype="application/json")
+    if not check_time_hall(data['week'], data['time'], data['hall']):
+        cursor = g.conn.execute('INSERT INTO instruction VALUES(%s,%s,%s,%s,%s)', data['coaid'], data['course'],
+                                data['hall'], data['week'], data['time'])
+        resp = Response(response=json.dumps([{'result': 'Success'}]), status=200, mimetype="application/json")
     else:
-        resp = Response(response=json.dumps([{'result':'Failed'}]),status=200, mimetype="application/json")
-    return(resp)
+        resp = Response(response=json.dumps([{'result': 'Failed'}]), status=200, mimetype="application/json")
+    return (resp)
 
 
-def check_time_hall(week,time, hall):
-    cursor = g.conn.execute('SELECT * from instruction where week = %s and time = %s and hallname = %s', week,time,hall)
+def check_time_hall(week, time, hall):
+    cursor = g.conn.execute('SELECT * from instruction where week = %s and time = %s and hallname = %s', week, time,
+                            hall)
     existed = False
-    for row in cursor: 
+    for row in cursor:
         existed = True
     return existed
 
-@routes.route('/coach/getinstruction',methods=['POST'])
+
+@routes.route('/coach/getinstruction', methods=['POST'])
 def getinstruction():
     data = request.get_json()
     result = []
     table_names = table_name_extracter('instruction')
-    cursor = g.conn.execute('SELECT * from instruction where coaid = %s',data['coaid'])
+    cursor = g.conn.execute('SELECT * from instruction where coaid = %s', data['coaid'])
     for row in cursor:
         temp = {}
-        for i in range(len(table_names)):            
-            temp[table_names[i]]=row[i]
+        for i in range(len(table_names)):
+            temp[table_names[i]] = row[i]
         result.append(temp)
-    resp = Response(response=json.dumps(result),status=200, mimetype="application/json")
-    return(resp)
+    resp = Response(response=json.dumps(result), status=200, mimetype="application/json")
+    return (resp)
 
-@routes.route('/coach/insmat',methods=['GET'])
+
+@routes.route('/coach/insmat', methods=['GET'])
 def getcourse():
     cursor = g.conn.execute('SELECT cid,name from course;')
     data1 = []
     for row in cursor:
-        data1.append(row[0]+':'+row[1])
-    data1 = {'course':data1}
+        data1.append(row[0] + ':' + row[1])
+    data1 = {'course': data1}
     cursor = g.conn.execute('SELECT name from hall;')
     data2 = []
     for row in cursor:
         data2.append(row[0])
-    data2 = {'hall':data2}
-    data3 = {'week':range(1, 8)}
-    data4 = {'time':range(19,23)}
-    resp = Response(response=json.dumps([data1,data2,data3,data4]),status=200, mimetype="application/json")
-    return(resp)
+    data2 = {'hall': data2}
+    data3 = {'week': range(1, 8)}
+    data4 = {'time': range(19, 23)}
+    resp = Response(response=json.dumps([data1, data2, data3, data4]), status=200, mimetype="application/json")
+    return (resp)
 
-@routes.route('/coach/delinstruction',methods=['DELETE'])
+
+@routes.route('/coach/delinstruction', methods=['DELETE'])
 def delinstruction():
     data = request.get_json()
-    cursor = g.conn.execute('delete from instruction where coaid = %s and time = %s and week = %s',data['coaid'], data['time'],data['week'])
-    resp = Response(response=json.dumps([{"result":"success"}]),status=200, mimetype="application/json")
-    return(resp)
+    cursor = g.conn.execute('delete from instruction where coaid = %s and time = %s and week = %s', data['coaid'],
+                            data['time'], data['week'])
+    resp = Response(response=json.dumps([{"result": "success"}]), status=200, mimetype="application/json")
+    return (resp)
